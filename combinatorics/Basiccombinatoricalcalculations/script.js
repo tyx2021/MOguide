335ac9325980c2a55b88dae01878d7cdd15ed622
tyx2021 <yixiangtey@gmail.com>
2022-03-18 21:24:38 +0800

    First commit

--- combinatorics/Basiccombinatoricalcalculations/script.js
+++
@@ -1,8 +0,0 @@
-function factorial() {
-    x=document.getElementById('factorialinput').value
-    output=1
-    for(i=1;i<=x;i++){
-        output=output*i
-    }
-    document.getElementById('factorialoutput').innerHTML="The factorial of "+x+" is "+ output
-    }
